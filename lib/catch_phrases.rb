28b3f7de6a9dc4976519145271aff455bd3594ad
Rick Glascock <rixong@gmail.com>
2020-01-08 22:18:43 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++
@@ -1,8 +0,0 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts "It's-a me, Mario!"
-end
-
-def toadstool
-  puts 'Thank You Mario! But Our Princess Is In Another Castle!'
-end
